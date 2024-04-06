cd907928342d9d84dcf90fc9ab5d448f44b29ad6
Ayoub Benramdane <ayoubbenramdane05@gmail.com>
2024-04-06 17:08:04 +0000

    new

--- brackets.go
+++ brackets.go
@@ -12,53 +12,56 @@ func main() {
 	}
 	res := []string{}
 	res1 := [][]string{}
-	resFinal := "OK"
+	resFinal := []string{}
 	for i := 0; i < len(args); i++ {
 		for j := 0; j < len(args[i]); j++ {
 			if args[i][j] == '(' || args[i][j] == '{' || args[i][j] == '[' || args[i][j] == ')' || args[i][j] == '}' || args[i][j] == ']' {
 				res = append(res, string(args[i][j]))
 			}
 		}
-		if res != nil {
-			res1 = append(res1, res)
+		if len(args[i]) == 0 {
+			res1 = append(res1, nil)
 		} else {
-			res1 = append(res1, []string{})
+			res1 = append(res1, nil)
 		}
 		res = []string{}
 	}
-	fmt.Println(res1)
+
 	for i := 0; i < len(res1); i++ {
-		if len(res1[i]) == 0 {
-			fmt.Println("OK")
-		} else {
-			for i := 0; i < len(res); i++ {
-				if res[i] == ")" {
-					if res[i-1] == "(" {
-						res = append(res[:i-1], res[i+1:]...)
-						i -= 2
-					} else {
-						resFinal = "Error"
-						break
-					}
-				} else if res[i] == "}" {
-					if res[i-1] == "{" {
-						res = append(res[:i-1], res[i+1:]...)
-						i -= 2
-					} else {
-						resFinal = "Error"
-						break
-					}
-				} else if res[i] == "]" {
-					if res[i-1] == "[" {
-						res = append(res[:i-1], res[i+1:]...)
-						i -= 2
-					} else {
-						resFinal = "Error"
-						break
-					}
+		if len(res1[i])%2 != 0 {
+			resFinal = append(resFinal, "Error")
+			continue
+		}
+		for i := 0; i < len(res); i++ {
+			if res[i] == ")" {
+				if res[i-1] == "(" {
+					res = append(res[:i-1], res[i+1:]...)
+					i -= 2
+				} else {
+					resFinal = append(resFinal, "Error")
+					break
+				}
+			} else if res[i] == "}" {
+				if res[i-1] == "{" {
+					res = append(res[:i-1], res[i+1:]...)
+					i -= 2
+				} else {
+					resFinal = append(resFinal, "Error")
+					break
+				}
+			} else if res[i] == "]" {
+				if res[i-1] == "[" {
+					res = append(res[:i-1], res[i+1:]...)
+					i -= 2
+				} else {
+					resFinal = append(resFinal, "Error")
+					break
 				}
 			}
 		}
+		resFinal = append(resFinal, "OK")
 	}
-	fmt.Println(resFinal)
+	for i := 0; i < len(resFinal); i++ {
+		fmt.Println(resFinal[i])
+	}
 }
