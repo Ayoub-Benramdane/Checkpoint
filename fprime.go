1c8ebe9e8177df113ee5207defabc429d641208a
Ayoub Benramdane <145620173+Ayoub-Benramdane@users.noreply.github.com>
2024-07-08 21:48:23 +0100

    fprime.go

--- fprime.go
+++ fprime.go
@@ -11,49 +11,34 @@ func main() {
 	if len(args) != 1 {
 		return
 	}
-	num, err := strconv.Atoi(args[0])
-	if err != nil || num <= 1 {
+	n, err := strconv.Atoi(args[0])
+	if err != nil || n <= 1 {
 		return
 	}
-	primes(num)
+	Fprime(n)
 }
 
-func primes(n int) {
-	for n%2 == 0 {
-		if n/2 != 1 {
-			fmt.Print("2*")
-		} else {
-			fmt.Print("2")
-		}
-		n /= 2
-	}
-	for i := 3; i <= n; i += 2 {
+func Fprime(n int) {
+	for i := 2;n/i != 0;i++ {
 		if n%i == 0 {
 			if n/i != 1 {
 				fmt.Printf("%d*", i)
-				n /= i
-				i = 3
+			} else {
+				fmt.Printf("%d", i)
 			}
+			n /= i
+			i--
 		}
 	}
-	if isPrime(n) {
-		fmt.Println(n)
-		return
-	}
+	fmt.Println()
 }
 
-func isPrime(nb int) bool {
-	if nb <= 1 {
+func isPrime(n int) bool {
+	if n <= 1 {
 		return false
 	}
-	if nb == 2 {
-		return true
-	}
-	if nb%2 == 0 {
-		return false
-	}
-	for i := 3; i <= nb/3; i += 2 {
-		if nb%i == 0 {
+	for i := 2; i*i <= n; i++ {
+		if n%i == 0 {
 			return false
 		}
 	}
