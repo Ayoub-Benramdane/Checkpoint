0e356faf3a6f726cb0b11ddbf4b51f5dcd3973fd
Ayoub Benramdane <ayoubbenramdane05@gmail.com>
2024-07-09 17:56:55 +0100

    new

--- findprevprime.go
+++ findprevprime.go
@@ -3,16 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(FindPrevPrime(5))
+	//fmt.Println(FindPrevPrime(225225))
 	fmt.Println(FindPrevPrime(28))
 }
 
+var c = 0
+
 func FindPrevPrime(nb int) int {
+	c++
 	for i := 2; i < nb; i++ {
 		if nb%i == 0 {
+			fmt.Println(c, nb)
 			nb -= 1
+			i = 2
 			FindPrevPrime(nb)
 		}
+		if nb%i != 0 && i+1 == nb {
+			return nb
+			break
+		}
+
 	}
 	return nb
 }
